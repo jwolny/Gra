0e289ac4e2ef7c1660107501995257af83660bd1
Szymon Gaczoł <gaczol.szymon@gmail.com>
2024-05-27 10:21:39 +0200

    player i bomb zrefaktoryzowane do mvvm

--- core/src/com/mygdx/game/model/MapHelper.java
+++ core/src/com/mygdx/game/model/MapHelper.java
@@ -1,6 +1,8 @@
 package com.mygdx.game.model;
 
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.PolygonMapObject;
@@ -10,65 +12,106 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.Shape;
-import com.mygdx.game.BodyPlayer;
+import com.badlogic.gdx.physics.box2d.*;
 import com.mygdx.game.GameScreen;
-import com.mygdx.game.Player;
+import com.mygdx.game.viewmodel.PlayerViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mygdx.game.model.Constants.*;
 
 public class MapHelper {
     TiledMap mapa;
     GameScreen gameScreen;
+    SpriteBatch spriteBatch;
+    Texture texture;
+    List<Body> bodies;
 
     public MapHelper(GameScreen gameScreen) {
         this.gameScreen = gameScreen;
+        this.spriteBatch = new SpriteBatch();
+        this.texture = new Texture("wall.png");
+        this.bodies = new ArrayList<Body>();
     }
 
     public OrthogonalTiledMapRenderer setUpMap() {
         mapa = new TmxMapLoader().load("mapa1_pop.tmx");
         parseMapObject(mapa.getLayers().get("Objects").getObjects());
+        generateRandomMap();
         return new OrthogonalTiledMapRenderer(mapa);
     }
-    public MapHelper() {
-        super();
+
+    public void render() {
+        spriteBatch.begin();
+        for (Body body : bodies) {
+            float xPos = (body.getPosition().x * PPM - PPM/2);
+            float yPos = (body.getPosition().y * PPM);
+            spriteBatch.draw(texture, xPos, yPos, 31, 31);
+        }
+        spriteBatch.end();
     }
 
+    private void generateRandomMap() {
+        GameOfLife gameOfLife = new GameOfLife();
+        boolean[][] cellmap = gameOfLife.generateMap();
+        cellmap = MapCollision.collision(cellmap);
+
+        for (int i = 0; i < tile_width; i++) {
+            for (int j = 0; j < tile_height; j++) {
+                if(cellmap[i][j]) {
+                    setRectangleBody(i, j, 32f, 32f);
+                }
+            }
+        }
+    }
+
+    private void setRectangleBody(int x, int y, float szerokosc, float wysokosc) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.position.set((x * szerokosc + szerokosc / 2) / PPM, (y * wysokosc + wysokosc / 2) / PPM);
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        Body body = gameScreen.getWorld().createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(szerokosc / 2 / PPM, wysokosc / 2 / PPM);
+        body.createFixture(shape, 0.0f);
+        shape.dispose();
+
+        bodies.add(body);
+    }
 
     private void parseMapObject(MapObjects mapObjects) {
         for (MapObject mapObject : mapObjects) {
-            if (mapObject instanceof PolygonMapObject)
-                creatStaticBody((PolygonMapObject) mapObject);
+            if (mapObject instanceof PolygonMapObject) {
+                createStaticBody((PolygonMapObject) mapObject);
+            }
 
             if (mapObject instanceof RectangleMapObject) {
                 Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
                 String rectangleName = mapObject.getName();
-                if (rectangleName.equals("player")) {
+                if (rectangleName != null && rectangleName.equals("player")) {
                     Body body = BodyPlayer.createBody(
                             rectangle.getX() + rectangle.getWidth() / 2,
                             rectangle.getY() + rectangle.getHeight() / 2,
-                            rectangle.getWidth(),
-                            rectangle.getHeight(),
+                            PPM - 3,    // ta szerokosc i wysokosc z mapy jest bez sensu imo ten moze chodzic normalnie
+                            PPM - 3,
                             gameScreen.getWorld()
                     );
                     Body body2 = BodyPlayer.createBody(
-                            100f,
-                            100f,
-                            rectangle.getWidth(),
-                            rectangle.getHeight(),
+                            3*PPM,
+                            2*PPM,
+                            PPM - 3,
+                            PPM - 3,
                             gameScreen.getWorld()
                     );
-                    gameScreen.setPlayer(new Player(rectangle.getWidth(), rectangle.getHeight(), body, Input.Keys.UP, Input.Keys.DOWN, Input.Keys.LEFT, Input.Keys.RIGHT, Input.Keys.SPACE, gameScreen.getWorld()));
-                    gameScreen.setPlayer(new Player(1f, 1f, body2, Input.Keys.W, Input.Keys.S, Input.Keys.A, Input.Keys.D, Input.Keys.F, gameScreen.getWorld()));
+                    gameScreen.setPlayer(new PlayerViewModel(new Player(rectangle.getWidth(), rectangle.getHeight(), body, Input.Keys.UP, Input.Keys.DOWN, Input.Keys.LEFT, Input.Keys.RIGHT, Input.Keys.SPACE, gameScreen.getWorld(),100.0f)));
+                    gameScreen.setPlayer(new PlayerViewModel(new Player(1f, 1f, body2, Input.Keys.W, Input.Keys.S, Input.Keys.A, Input.Keys.D, Input.Keys.F, gameScreen.getWorld(),100.0f)));
                 }
             }
         }
-
     }
 
-    private void creatStaticBody(PolygonMapObject polygonMapObject) {
+    private void createStaticBody(PolygonMapObject polygonMapObject) {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.StaticBody;
         Body body = gameScreen.getWorld().createBody(bodyDef);
@@ -78,16 +121,30 @@ public class MapHelper {
     }
 
     private Shape createPolygonShape(PolygonMapObject polygonMapObject) {
-        float[] verticies = polygonMapObject.getPolygon().getTransformedVertices();
-        Vector2[] worldVerticies = new Vector2[verticies.length / 2];
+        float[] vertices = polygonMapObject.getPolygon().getTransformedVertices();
+        Vector2[] worldVertices = new Vector2[vertices.length / 2];
 
-        for (int i = 0; i < verticies.length / 2; i++) {
-            Vector2 curr = new Vector2(verticies[i * 2] / 32.0f, verticies[i * 2 + 1] / 32.0f);
-            worldVerticies[i] = curr;
+        for (int i = 0; i < vertices.length / 2; i++) {
+            Vector2 curr = new Vector2(vertices[i * 2] / PPM, vertices[i * 2 + 1] / PPM);
+            worldVertices[i] = curr;
         }
 
         PolygonShape shape = new PolygonShape();
-        shape.set(worldVerticies);
+        shape.set(worldVertices);
         return shape;
+    }
+
+    public void removeBody(Body body) {
+        gameScreen.getWorld().destroyBody(body);
+        bodies.remove(body);
+    }
+
+    public void dispose() {
+        mapa.dispose();
+        spriteBatch.dispose();
+        texture.dispose();
+        for (Body body : bodies) {
+            gameScreen.getWorld().destroyBody(body);
+        }
     }
 }
