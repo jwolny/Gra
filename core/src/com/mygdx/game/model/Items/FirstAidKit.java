0d21e1d99386ac74d20a2e189d741e6e32c811db
jwolny <jakub1.wolny@studen.uj.edu.pl>
2024-06-16 21:38:48 +0200

    Random Map

--- core/src/com/mygdx/game/model/Items/FirstAidKit.java
+++ core/src/com/mygdx/game/model/Items/FirstAidKit.java
@@ -1,13 +1,10 @@
 package com.mygdx.game.model.Items;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.model.PlayerTools.Player;
 
-import static com.mygdx.game.others.Constants.*;
+import static com.mygdx.game.others.Constants.PPM;
 
 public class FirstAidKit extends Items {
     public FirstAidKit(World world, float x, float y) {
@@ -15,26 +12,7 @@ public class FirstAidKit extends Items {
         velocity = new Vector2(0, 0);
         //setSize(32 / PPM, 32 / PPM);
     }
-/*
-    @Override
-    public void defineItem() {
-        BodyDef bdef = new BodyDef();
-        bdef.position.set(getX(), getY());
-        bdef.type = BodyDef.BodyType.DynamicBody;
-        body = world.createBody(bdef);
 
-        FixtureDef fdef = new FixtureDef();
-        CircleShape shape = new CircleShape();
-        shape.setRadius(10 / PPM);
-        fdef.filter.categoryBits = ITEM_BIT;
-        fdef.density = 0;
-        fdef.filter.maskBits = PLAYER_BIT;
-
-        fdef.shape = shape;
-        body.createFixture(fdef).setUserData(this);
-        shape.dispose();
-    }
-*/
     @Override
     public void use(Player player) {
         destroy();
--- core/src/com/mygdx/game/model/Items/SpeedPotion.java
+++ core/src/com/mygdx/game/model/Items/SpeedPotion.java
@@ -1,14 +1,11 @@
 package com.mygdx.game.model.Items;
 
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Timer;
 import com.mygdx.game.model.PlayerTools.Player;
 
-import static com.mygdx.game.others.Constants.*;
+import static com.mygdx.game.others.Constants.PPM;
 
 public class SpeedPotion extends Items {
     public SpeedPotion(World world, float x, float y) {
@@ -16,27 +13,6 @@ public class SpeedPotion extends Items {
         velocity = new Vector2(0, 0);
         //setSize(32 / PPM, 32 / PPM);
     }
-/*
-    @Override
-    public void defineItem() {
-        BodyDef bdef = new BodyDef();
-        bdef.position.set(getX(), getY());
-        bdef.type = BodyDef.BodyType.DynamicBody;
-        body = world.createBody(bdef);
-
-        FixtureDef fdef = new FixtureDef();
-        CircleShape shape = new CircleShape();
-        shape.setRadius(10 / PPM);
-        fdef.filter.categoryBits = ITEM_BIT;
-        fdef.density = 0;
-        fdef.filter.maskBits = PLAYER_BIT;
-
-        fdef.shape = shape;
-        body.createFixture(fdef).setUserData(this);
-        shape.dispose();
-    }
-
- */
 
     @Override
     public void use(Player player) {
